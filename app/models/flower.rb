0f7345651b16d27c476454e19ed8905b7d31646d
gourouhassiba <gourouhassiba@gmail.com>
2020-10-04 15:45:13 +0200

    modify index

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,5 +1,7 @@
 class Flower < ApplicationRecord
   belongs_to :category
   has_many :orders, dependent: :delete_all
-  monetize :price_cents
+  store_accessor :properties, :price_cents
+  monetize :price_cents, disable_validation: true
+  validates :price, money: true
 end

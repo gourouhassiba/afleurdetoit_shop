36f607de965a421ba8a6c34748f41384dcea753e
gourouhassiba <gourouhassiba@gmail.com>
2020-10-04 16:24:51 +0200

    delete accessor in the flower model

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,7 +1,6 @@
 class Flower < ApplicationRecord
   belongs_to :category
   has_many :orders, dependent: :delete_all
-  store_accessor :properties, :price_cents
   monetize :price_cents, disable_validation: true
   validates :price, money: true
 end

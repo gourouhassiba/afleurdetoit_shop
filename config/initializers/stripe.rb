796a361b997453c902523555777b424dc5a58c34
gourouhassiba <gourouhassiba@gmail.com>
2020-10-05 20:30:45 +0200

    add key to stripe.rbb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,11 @@
 require "stripe"
 Rails.configuration.stripe = {
+  :publishable_key => Rails.application.secrets.publishable_key,
+  :secret_key      => Rails.application.secrets.secret_key
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
   secret_key:      ENV['STRIPE_SECRET_KEY'],
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
